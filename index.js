6133d9f7d8d49f1305d7bb318286136696858019
Patrik Kumpula <patrik19100@student.hamk.fi>
2022-02-18 19:53:11 +0200

    changes for heroku

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.post("/addcustomer", (req, res) =>
     customers = [...customers, newCustomer];
     res.redirect("/");
 });
-app.listen(port, () => 
+app.listen(process.env.port || 3000, () => 
 {
   console.log(`Server is running on port ${port}.`);
 });
